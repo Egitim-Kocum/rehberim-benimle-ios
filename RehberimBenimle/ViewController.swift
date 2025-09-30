8479e249068c7813160efac1955496e7dc1d0018
abdulkerim.can <abdulkerimcann@outlook.com>
2025-09-30 18:15:26 +0300

    Remove Storyboard & Add gitignore

---
+++ RehberimBenimle/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RehberimBenimle
+//
+//  Created by Abdulkerim Can on 30.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
