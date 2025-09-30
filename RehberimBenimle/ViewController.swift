e8116ca3156de0033a76827110be4e5b4cbc78e2
abdulkerim.can <abdulkerimcann@outlook.com>
2025-09-30 18:39:19 +0300

    Add libraries

--- RehberimBenimle/ViewController.swift
+++ RehberimBenimle/ViewController.swift
@@ -6,12 +6,33 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "Rehberim Benimle"
+        label.textColor = .label
+        label.font = .preferredFont(forTextStyle: .headline)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.addSubviews(label)
+        view.backgroundColor = .systemBackground
+        
+        label.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
     }
 }
 
+
+extension UIView {
+    func addSubviews(_ view: UIView...) {
+        view.forEach { self.addSubview($0) }
+    }
+}
